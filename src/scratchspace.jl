292544b3956a498527fe9d3cc82fe8fc4d6f6e1d
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2025-05-10 11:47:07 +0900

    Allow to find old version of docstrings

--- src/scratchspace.jl
+++ src/scratchspace.jl
@@ -1,34 +1,56 @@
-function pathofcachedir(md::Markdown.MD)
+function prevminor(v::VersionNumber)
+    return VersionNumber(v.major, v.minor - 1, 0)
+end
+
+function insertversion(svec::Vector{String}, v::VersionNumber)
+    major = v.major
+    minor = v.minor
+    insert!(deepcopy(svec), 2, "$(major).$(minor)")
+end
+
+function pathofcachedir(md::Markdown.MD, allowold::Bool = false)
     if haskey(md.meta, :module)
         svec = split(string(md.meta[:module]), ".")
         v = something(pkgversion(md.meta[:module]), VERSION)
-        major = v.major
-        minor = v.minor
-        insert!(svec, 2, "$(major).$(minor)")
-        joinpath(TRANSLATION_CACHE_DIR[], svec...)
+        svec_with_version = insertversion(svec, v)
+        d = joinpath(TRANSLATION_CACHE_DIR[], svec_with_version...)
+        if !isdir(d) && allowold
+            # we try to find the previous minor version.
+            svec_with_prev_version = insertversion(deepcopy(svec), prevminor(v))
+            return joinpath(TRANSLATION_CACHE_DIR[], svec_with_prev_version...)
+        else
+            return d
+        end
     elseif haskey(md.meta, :path)
         # In case the module is not set.
         # This happens when we translate markdown in doc/src/<blah>.md
         svec = splitpath(md.meta[:path])
         v = VERSION
-        major = v.major
-        minor = v.minor
-        insert!(svec, 2, "$(major).$(minor)")
-        joinpath(TRANSLATION_CACHE_DIR[], svec...)
+        svec_with_version = insertversion(svec, v)
+        d = joinpath(TRANSLATION_CACHE_DIR[], svec_with_version...)
+        if !isdir(d) && allowold
+            # we try to find the previous minor version.
+            svec_with_prev_version = insertversion(deepcopy(svec), prevminor(v))
+            return joinpath(TRANSLATION_CACHE_DIR[], svec_with_prev_version...)
+        else
+            return d
+        end
     else
-        return TRANSLATION_CACHE_DIR[]
+        throw(ArgumentError("No module or path found in the markdown metadata."))
     end
 end
 
 function istranslated(md::Markdown.MD)
-    cachedir = joinpath(pathofcachedir(md), hashmd(md))
+    allowold = true
+    cachedir = joinpath(pathofcachedir(md, allowold), hashmd(md))
     lang = DEFAULT_LANG[]
     mdpath = joinpath(cachedir, lang * ".md")
     isfile(mdpath)
 end
 
 function load_translation(md::Markdown.MD)
-    cachedir = joinpath(pathofcachedir(md), hashmd(md))
+    allowold = true
+    cachedir = joinpath(pathofcachedir(md, allowold), hashmd(md))
     lang = DEFAULT_LANG[]
     mdpath = joinpath(cachedir, lang * ".md")
     Markdown.parse(postprocess_content(read(mdpath, String)))
